89374fa87e6a45597898c6707bf1c223008cd910
Johnny <johnny@mail.mail>
2024-01-05 09:12:28 +0100

    Update to zig 0.11 and ESP-IDF v5.1.2
    Tested on Lolin C3 Mini board with plain LED
    Printing to USB UART also works but printing "tag" inside
    esp_log_write() doesn't.
    LED strip code removed as it can't be used currently from Zig - not
    possible to use bitfields inside of C structs (see #1499)

--- main/src/log.zig
+++ main/src/log.zig
@@ -7,9 +7,12 @@ const esp = @cImport({
 const COLOR_FORMAT = "\x1b[0;{d}m";
 const COLOR_RESET = "\x1b[0m";
 const COLOR_GREEN = 32;
+const COLOR_CYAN = 36;
 
 pub fn info(comptime tag: [:0]const u8, comptime format: [:0]const u8, args: anytype) void {
-    const fmt = std.fmt.comptimePrint(COLOR_FORMAT ++ "I (%u): {s}" ++ COLOR_RESET ++ "\n", .{COLOR_GREEN, format});
+
+
+    const fmt = std.fmt.comptimePrint(COLOR_FORMAT ++ "I (%u): {s}" ++ COLOR_RESET ++ "\n", .{COLOR_CYAN, format});
     const timestamp = esp.esp_log_timestamp();
-    @call(.{}, esp.esp_log_write, .{esp.ESP_LOG_INFO, tag, fmt, timestamp} ++ args);
+    @call(.auto, esp.esp_log_write, .{esp.ESP_LOG_INFO, tag, fmt, timestamp} ++ args);
 }
--- main/src/main.zig
+++ main/src/main.zig
@@ -8,7 +8,7 @@ const c = @cImport({
 const esp = @cImport({
     @cInclude("sdkconfig.h");
     @cInclude("esp_system.h");
-    @cInclude("led_strip.h");
+    @cInclude("driver/gpio.h");
 });
 
 const freertos = @cImport({
@@ -16,82 +16,21 @@ const freertos = @cImport({
     @cInclude("freertos/task.h");
 });
 
-const BLINK_LED_RMT_CHANNEL = esp.CONFIG_BLINK_LED_RMT_CHANNEL;
-const BLINK_GPIO = esp.CONFIG_BLINK_GPIO;
+const BLINK_GPIO = 5;	// plain old LED
 
-const LedStrip = struct {
-    ptr: *esp.led_strip_t,
-
-    const Self = @This();
-
-    pub fn init(channel: u8, gpio: u8, led_num: u16) Self {
-        var self = Self { .ptr = esp.led_strip_init(channel, gpio, led_num) };
-        self.clear(50) catch unreachable;
-        return self;
-    }
-
-    pub fn clear(self: Self, timeout: u32) !void {
-        return switch (self.ptr.clear.?(self.ptr, timeout)) {
-            esp.ESP_ERR_TIMEOUT => {
-                _ = c.printf("Clear LEDs failed because of timeout\n");
-                return error.Timeout;
-            },
-            esp.ESP_FAIL => {
-                _ = c.printf("Clear LEDs failed because some other error occurred\n");
-                return error.Other;
-            },
-            else => .{},
-        };
-    }
-
-    pub fn setPixel(self: Self, index: u32, red: u32, green: u32, blue: u32) !void {
-        return switch (self.ptr.set_pixel.?(self.ptr, index, red, green, blue)) {
-            esp.ESP_ERR_INVALID_ARG => {
-                _ = c.printf("Set RGB for a specific pixel failed because of invalid parameters\n");
-                return error.InvalidArgument;
-            },
-            esp.ESP_FAIL => {
-                _ = c.printf("Set RGB for a specific pixel failed because other error occurred\n");
-                return error.Other;
-            },
-            else => .{},
-        };
-    }
-
-    pub fn refresh(self: Self, timeout: u32) !void {
-        return switch (self.ptr.refresh.?(self.ptr, timeout)) {
-            esp.ESP_ERR_TIMEOUT => {
-                _ = c.printf("Refresh failed because of timeout\n");
-                return error.Timeout;
-            },
-            esp.ESP_FAIL => {
-                _ = c.printf("Refresh failed because other error occurred\n");
-                return error.Other;
-            },
-            else => .{},
-        };
-    }
-
-    pub fn enable(self: Self) void {
-        self.setPixel(0, 16, 16, 16) catch unreachable;
-        self.refresh(100) catch unreachable;
-    }
-
-    pub fn disable(self: Self) void {
-        self.clear(50) catch unreachable;
-    }
-};
+const TAG = "[zig main]";
 
 export fn app_main() void {
     var led_state = false;
-    var strip = LedStrip.init(BLINK_LED_RMT_CHANNEL, BLINK_GPIO, 1);
+    _= esp.gpio_reset_pin(BLINK_GPIO);
+    _= esp.gpio_set_direction(BLINK_GPIO, esp.GPIO_MODE_OUTPUT);
 
     while (true) {
-        log.info("blink", "Toggling the LED %s!", .{@as([:0]const u8, if (led_state) "ON" else "OFF").ptr});
+        log.info(TAG, "Toggling the LED %s!", .{@as([:0]const u8, if (led_state) "ON" else "OFF").ptr});
         if (led_state) {
-            strip.enable();
+            _ = esp.gpio_set_level(BLINK_GPIO, 1);
         } else {
-            strip.disable();
+            _ = esp.gpio_set_level(BLINK_GPIO, 0);
         }
         led_state = !led_state;
         freertos.vTaskDelay(esp.CONFIG_BLINK_PERIOD / freertos.portTICK_PERIOD_MS);
